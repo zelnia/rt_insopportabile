41ad539841a141064b34d92708cefb1832b139df
S <380gradi@gmail.com>
2016-09-28 00:57:35 +0200

    dramatic platform

---
+++ platforms/android/assets/www/supporto.php
@@ -0,0 +1,10 @@
+ 
+	  <?php 	  
+	    $username = $_POST["username"];
+		$password = $_POST["password"];
+		if ($username == "prova" && $password =="prova"){
+			echo "#page1";
+		}
+
+      //header('Location: http://www.spatarodetergenza.com/crm/ruoloa/gestione_ordini_corrado.php');
+	  ?>
---
+++ platforms/browser/www/js/script.js
@@ -0,0 +1,90 @@
+$(document).ready(function(){	
+	/*$("#submit-6").click(function(){
+        $.ajax({url: "http://www.tanadeldrago.it/signals/supporto.php", success: function(result){
+			window.location(result);
+        }});
+    });
+	
+	function connect(usr,pwd)
+	  {
+	      jQuery.ajax({
+	       type: "POST",
+		   dataType: 'jsonp',
+	       url: "http://www.tanadeldrago.it/signals/supporto.php",
+	       data: {username:'prova',password:'prova'},
+	       //data: 'idordine='+id,'statoordine='+stato,
+	       cache: false,
+	       success: function(response)
+	       {
+	        //location.reload();
+	        window.location(response);
+	       }
+	     });
+	 }
+	
+	
+	
+		var Mostra1 = "";
+var StringaXml1 = signal_1.XMLDocument;
+var i;
+for (i=0; i<StringaXml1.documentElement.childNodes.length; i++) {
+ Mostra += "<table bgcolor='#F9F9F9' align='center' width='450' border='1' bordercolor='#CCCCCC'>"
+ Mostra += "<tr>"
+ Mostra += "<td colspan='2' style='background-color: #008080;'>"
+ Mostra += "<font color='#FFFFFF'><b>Amico # " + (i + 1) + "</b></font>"
+ Mostra += "</td>"
+ Mostra += "</tr>"
+ Mostra += "<tr>"
+ Mostra += "<td width='100'><b>Nome:</b></td>"
+ Mostra += "<td>" + StringaXml.documentElement.childNodes(i).childNodes(0).text + "</td>"
+ Mostra += "</tr>"
+ Mostra += "<tr>"
+ Mostra += "<td><b>Cognome:</b></td>"
+ Mostra += "<td>" + StringaXml.documentElement.childNodes(i).childNodes(1).text + "</td>"
+ Mostra += "</tr>"
+ Mostra += "<tr>"
+ Mostra += "<td><b>Telefono:</b></td>"
+ Mostra += "<td>" + StringaXml.documentElement.childNodes(i).childNodes(2).text + "</td>"
+ Mostra += "</tr>"
+ Mostra += "<tr>"
+ Mostra += "<td><b>Indirizzo:</b></td>"
+ Mostra += "<td>" + StringaXml.documentElement.childNodes(i).childNodes(3).text + "</td>"
+ Mostra += "</tr>"
+ Mostra += "<tr>"
+ Mostra += "<td><b>Città:</b></td>"
+ Mostra += "<td>" + StringaXml.documentElement.childNodes(i).childNodes(4).text + "</td>"
+ Mostra += "</tr>"
+ Mostra += "<tr>"
+ Mostra += "<td><b>Città:</b></td>"
+ Mostra += "<td>" + StringaXml.documentElement.childNodes(i).childNodes(5).text + "</td>"
+ Mostra += "</tr>"
+ Mostra += "<tr>"
+ Mostra += "<td><b>Città:</b></td>"
+ Mostra += "<td>" + StringaXml.documentElement.childNodes(i).childNodes(6).text + "</td>"
+ Mostra += "</tr>"
+ Mostra += "<tr>"
+ Mostra += "<td><b>Città:</b></td>"
+ Mostra += "<td>" + StringaXml.documentElement.childNodes(i).childNodes(7).text + "</td>"
+ Mostra += "</tr>"
+ Mostra += "<tr>"
+ Mostra += "<td><b>Città:</b></td>"
+ Mostra += "<td>" + StringaXml.documentElement.childNodes(i).childNodes(8).text + "</td>"
+ Mostra += "</tr>"
+ Mostra += "<tr>"
+ Mostra += "<td><b>Città:</b></td>"
+ Mostra += "<td>" + StringaXml.documentElement.childNodes(i).childNodes(9).text + "</td>"
+ Mostra += "</tr>"
+ Mostra += "<tr>"
+ Mostra += "<td><b>Città:</b></td>"
+ Mostra += "<td>" + StringaXml.documentElement.childNodes(i).childNodes(10).text + "</td>"
+ Mostra += "</tr>"
+ Mostra += "</table>"
+ Mostra += "<br>";
+}
+s1.innerHTML = Mostra1;
+	
+	*/
+	
+	
+});
+
